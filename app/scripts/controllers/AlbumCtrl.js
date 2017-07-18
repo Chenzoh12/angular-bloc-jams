61abd83980ee242ff181eab135a0699bf9f997c5
Vincent Scotti <scottivincentj@gmail.com>
2017-07-17 21:05:25 -0400

    End of 6. Works as described.

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,15 +1,8 @@
 (function() {
-<<<<<<< HEAD
-  function AlbumCtrl() {
-    this.albumData = albumPicasso;
-    }
-    
-=======
   function AlbumCtrl(Fixtures) {
       this.albumData = Fixtures.getAlbum();
     }
 
->>>>>>> ng-checkpoint-6
 
   angular
     .module('blocJams')
